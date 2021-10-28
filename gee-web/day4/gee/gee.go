5d2b25d5aac48beb4b24cbdbb90bc44b5947acb4
wujiahao <wujiahao.mail@gmail.com>
2021-10-28 10:10:15 +0800

    web day3

---
+++ gee-web/day4/gee/gee.go
@@ -0,0 +1,36 @@
+package gee
+
+import (
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+type Engine struct {
+	router *router
+}
+
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handlerFunc HandlerFunc) {
+	engine.router.addRoute(method, pattern, handlerFunc)
+}
+
+func (engine *Engine) GET(pattern string, handlerFunc HandlerFunc) {
+	engine.addRoute("GET", pattern, handlerFunc)
+}
+
+func (engine *Engine) POST(pattern string, handlerFunc HandlerFunc) {
+	engine.addRoute("POST", pattern, handlerFunc)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
