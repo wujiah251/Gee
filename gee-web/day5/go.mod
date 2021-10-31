406cd9509f9e25f6dabd10fc9943c057b20852a0
wujiahao <wujiahao.mail@gmail.com>
2021-10-31 14:03:01 +0800

    web day5

---
+++ gee-web/day5/go.mod
@@ -0,0 +1,5 @@
+module Gee/gee-web/day5
+
+go 1.16
+
+require github.com/sirupsen/logrus v1.8.1
--- gee-web/day5/main.go
+++ gee-web/day5/main.go
@@ -1,1 +1,34 @@
-package day3
+package main
+
+import (
+	"Gee/gee-web/day5/gee"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+func onlyFormV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		t := time.Now()
+		c.Fail(500, "Internal Server Error")
+		logrus.Printf("[%d] %s int %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
+func main() {
+	r := gee.New()
+	r.Use(gee.Logger())
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	v2 := r.Group("/v2")
+	v2.Use(onlyFormV2())
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
+	r.Run(":9999")
+}
