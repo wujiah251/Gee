60c709221b52c785977e5c28b0cf53a8f664f995
wujiahao <wujiahao.mail@gmail.com>
2021-11-01 23:26:12 +0800

    geecache day3

---
+++ gee-cache/day3/geecache/cache.go
@@ -0,0 +1,35 @@
+package geecache
+
+import (
+	"Gee/gee-cache/day3/geecache/lru"
+	"sync"
+)
+
+// 并发控制
+
+type cache struct {
+	sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.Lock()
+	defer c.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.Lock()
+	defer c.Unlock()
+	if c.lru == nil {
+		return
+	}
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
